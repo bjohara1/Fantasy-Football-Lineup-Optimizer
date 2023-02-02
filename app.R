906c7bd912ed67131771c5f73a3c7dd993857d4b
Brad <78824797+yankeefool11@users.noreply.github.com>
2023-02-02 10:12:42 -0500

    Add files via upload

---
+++ app.R
@@ -0,0 +1,187 @@
+rsconnect::setAccountInfo(name='fantasyanalytics', token='C2AB37E235CB97095C45A22804471B84', secret='EWdGwFby/nHaTEZA6LGMudWdcbVpDxPHwSvpOmSU')
+
+players <- structure(list(Player = c("Josh Allen", "Patrick Mahomes", "Justin Herbert", 
+                                     "Lamar Jackson", "Kyler Murray", "Jalen Hurts", "Tom Brady", 
+                                     "Dak Prescott", "Joe Burrow", "Russell Wilson", "Aaron Rodgers", 
+                                     "Trey Lance", "Matthew Stafford", "Kirk Cousins", "Derek Carr", 
+                                     "Tua Tagovailoa", "Justin Fields", "Trevor Lawrence", "Ryan Tannehill", 
+                                     "Daniel Jones", "Matt Ryan", "Jameis Winston", "Carson Wentz", 
+                                     "Mac Jones", "Jared Goff", "Zach Wilson", "Davis Mills", "Baker Mayfield", 
+                                     "Marcus Mariota", "Deshaun Watson", "Mitchell Trubisky", "Geno Smith", 
+                                     "Drew Lock", "Kenny Pickett", "Jacoby Brissett", "Desmond Ridder", 
+                                     "Travis Kelce", "Mark Andrews", "Kyle Pitts", "Darren Waller", 
+                                     "George Kittle", "Dalton Schultz", "T.J. Hockenson", "Dallas Goedert", 
+                                     "Zach Ertz", "Dawson Knox", "Hunter Henry", "Mike Gesicki", "Pat Freiermuth", 
+                                     "Cole Kmet", "Irv Smith Jr.", "Noah Fant", "Tyler Higbee", "David Njoku", 
+                                     "Albert Okwuegbunam", "Gerald Everett", "Robert Tonyan", "Jonathan Taylor", 
+                                     "Christian McCaffrey", "Derrick Henry", "Austin Ekeler", "Dalvin Cook", 
+                                     "Joe Mixon", "Najee Harris", "Alvin Kamara", "D'Andre Swift", 
+                                     "Leonard Fournette", "Saquon Barkley", "Aaron Jones", "Nick Chubb", 
+                                     "James Conner", "Javonte Williams", "Ezekiel Elliott", "David Montgomery", 
+                                     "Cam Akers", "Travis Etienne Jr.", "Breece Hall", "J.K. Dobbins", 
+                                     "Josh Jacobs", "Antonio Gibson", "Elijah Mitchell", "AJ Dillon", 
+                                     "Cordarrelle Patterson", "Damien Harris", "Miles Sanders", "Clyde Edwards-Helaire", 
+                                     "Tony Pollard", "Devin Singletary", "Kareem Hunt", "Chase Edmonds", 
+                                     "Rashaad Penny", "Rhamondre Stevenson", "Kenneth Walker III", 
+                                     "Melvin Gordon III", "Darrell Henderson Jr.", "James Robinson", 
+                                     "James Cook", "Dameon Pierce", "Michael Carter", "Jamaal Williams", 
+                                     "Nyheim Hines", "J.D. McKissic", "Kenneth Gainwell", "Alexander Mattison", 
+                                     "Isaiah Spiller", "Raheem Mostert", "Mark Ingram II", "Marlon Mack", 
+                                     "Brian Robinson", "Gus Edwards", "Rex Burkhead", "Rachaad White", 
+                                     "Khalil Herbert", "Damien Williams", "Tyler Allgeier", "D'Onta Foreman", 
+                                     "Jerick McKinnon", "Cooper Kupp", "Justin Jefferson", "Ja'Marr Chase", 
+                                     "Davante Adams", "Stefon Diggs", "Deebo Samuel", "CeeDee Lamb", 
+                                     "Mike Evans", "Tyreek Hill", "Tee Higgins", "Keenan Allen", "DJ Moore", 
+                                     "A.J. Brown", "Michael Pittman Jr.", "Mike Williams", "Brandin Cooks", 
+                                     "Jaylen Waddle", "Diontae Johnson", "Terry McLaurin", "DK Metcalf", 
+                                     "Courtland Sutton", "Amon-Ra St. Brown", "Darnell Mooney", "Allen Robinson II", 
+                                     "Marquise Brown", "Amari Cooper", "Gabriel Davis", "Chris Godwin", 
+                                     "Michael Thomas", "Jerry Jeudy", "Adam Thielen", "JuJu Smith-Schuster", 
+                                     "Hunter Renfrow", "Rashod Bateman", "Elijah Moore", "Tyler Lockett", 
+                                     "Christian Kirk", "Robert Woods", "DeVonta Smith", "Drake London", 
+                                     "Allen Lazard", "Brandon Aiyuk", "Chase Claypool", "Kadarius Toney", 
+                                     "Tyler Boyd", "Garrett Wilson", "DeVante Parker", "Chris Olave", 
+                                     "Kenny Golladay", "Jakobi Meyers", "Russell Gage", "Marquez Valdes-Scantling", 
+                                     "DeAndre Hopkins", "Marvin Jones Jr.", "Treylon Burks", "Michael Gallup", 
+                                     "Robbie Anderson", "DJ Chark", "Jahan Dotson", "Mecole Hardman"
+), Position = c("QB", "QB", "QB", "QB", "QB", "QB", "QB", "QB", 
+                "QB", "QB", "QB", "QB", "QB", "QB", "QB", "QB", "QB", "QB", "QB", 
+                "QB", "QB", "QB", "QB", "QB", "QB", "QB", "QB", "QB", "QB", "QB", 
+                "QB", "QB", "QB", "QB", "QB", "QB", "TE", "TE", "TE", "TE", "TE", 
+                "TE", "TE", "TE", "TE", "TE", "TE", "TE", "TE", "TE", "TE", "TE", 
+                "TE", "TE", "TE", "TE", "TE", "RB", "RB", "RB", "RB", "RB", "RB", 
+                "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", 
+                "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", 
+                "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", 
+                "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", 
+                "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", "RB", "WR", 
+                "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", 
+                "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", 
+                "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", 
+                "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", 
+                "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", "WR", 
+                "WR", "WR", "WR", "WR"), FantasyPoints = c(445, 410, 407, 348, 
+                                                           351, 359, 354, 364, 402, 368, 353, 347, 349, 335, 366, 325, 297, 
+                                                           313, 273, 283, 302, 284, 275, 296, 291, 0, 247, 286, 276, 0, 
+                                                           0, 0, 0, 269, 0, 0, 252, 231, 206, 171, 185, 177, 174, 169, 169, 
+                                                           171, 139, 131, 170, 170, 162, 129, 162, 119, 130, 126, 130, 340, 
+                                                           285, 260, 278, 277, 271, 277, 247, 271, 225, 247, 249, 230, 196, 
+                                                           268, 205, 199, 213, 231, 220, 177, 176, 159, 178, 185, 155, 181, 
+                                                           157, 190, 177, 164, 156, 166, 169, 179, 158, 129, 147, 99, 158, 
+                                                           176, 150, 100, 157, 128, 156, 124, 98, 95, 75, 90, 136, 80, 82, 
+                                                           143, 128, 0, 147, 97, 63, 326, 337, 308, 299, 269, 267, 271, 
+                                                           242, 243, 241, 239, 243, 242, 244, 209, 220, 233, 239, 221, 198, 
+                                                           221, 209, 220, 209, 218, 178, 224, 183, 186, 203, 188, 164, 207, 
+                                                           211, 202, 173, 188, 163, 199, 171, 181, 182, 140, 170, 175, 144, 
+                                                           142, 164, 147, 131, 170, 160, 182, 136, 153, 157, 152, 148, 175, 
+                                                           144), DraftValue = c(31, 23, 20, 15, 16, 14, 16, 11, 12, 10, 
+                                                                                10, 3, 7, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+                                                                                1, 1, 1, 1, 1, 1, 37, 34, 22, 20, 17, 16, 12, 11, 9, 6, 4, 4, 
+                                                                                5, 5, 2, 2, 2, 1, 1, 1, 1, 56, 55, 44, 48, 38, 38, 40, 38, 36, 
+                                                                                34, 34, 33, 27, 30, 28, 27, 23, 21, 23, 21, 19, 18, 10, 15, 16, 
+                                                                                16, 12, 12, 14, 13, 10, 11, 12, 8, 9, 1, 6, 1, 1, 1, 1, 1, 1, 
+                                                                                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 56, 48, 41, 
+                                                                                40, 37, 31, 34, 29, 30, 28, 28, 26, 24, 26, 23, 23, 22, 21, 20, 
+                                                                                18, 19, 20, 17, 18, 17, 15, 15, 17, 17, 16, 16, 15, 15, 13, 12, 
+                                                                                12, 12, 11, 9, 9, 9, 7, 5, 6, 4, 2, 2, 2, 1, 3, 1, 1, 1, 1, 1, 
+                                                                                1, 1, 1, 1, 1)), class = c("tbl_df", "tbl", "data.frame"), row.names = c(NA, 
+                                                                                                                                                         -177L))
+
+library(shiny)
+library(lpSolve)
+library(rsconnect)
+
+# Define the UI for the app
+ui <- fluidPage(
+  titlePanel("Fantasy Football Lineup Optimizer"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      numericInput("num_qb", "Enter the number of QBs:", 1),
+      numericInput("num_rb", "Enter the number of RBs:", 2),
+      numericInput("num_wr", "Enter the number of WRs:", 3),
+      numericInput("num_te", "Enter the number of TEs:", 1),
+      numericInput("num_value", "Enter your draft budget:", 200),
+      numericInput("num_players", "Adding in your flex spots, enter the total number of starters:", 9),
+      selectInput("remove", "Remove a player:", choices = c("",as.character(players$Player)), multiple = TRUE),
+      actionButton("update", "Update Team")
+    ),
+    mainPanel(
+      tableOutput("team")
+    )
+  )
+)
+
+# Define the server logic
+server <- function(input, output) {
+  players <- players
+  
+  # Create a new column indicating the player's position
+  players$QB <- ifelse(players$Position == "QB", 1, 0)
+  players$RB <- ifelse(players$Position == "RB", 1, 0)
+  players$WR <- ifelse(players$Position == "WR", 1, 0)
+  players$TE <- ifelse(players$Position == "TE", 1, 0)
+  players$Total <- 1
+  rv <- reactiveValues(players=players)
+  
+  # Define the objective function (maximize fantasy points)
+  obj <- players$FantasyPoints
+  
+  # Define the constraints (position limits and draft value limit)
+  con <- reactive({
+    matrix(c(
+      # QB constraint
+      rv$players$QB,
+      # RB constraint
+      rv$players$RB,
+      # WR constraint
+      rv$players$WR,
+      # TE constraint
+      rv$players$TE,
+      # Draft value constraint
+      rv$players$DraftValue,
+      #Total players constraint
+      rv$players$Total
+    ), ncol = nrow(rv$players), byrow = TRUE)
+  })
+  
+  
+  # Define the variables for the lp
+  dir <- c("<=", rep(">=",3),"<=","<=")
+  
+  # Define the initial optimal lineup
+  initialLineup <- reactive({
+    rhs <- reactive({
+      c(input$num_qb, input$num_rb, input$num_wr, input$num_te, input$num_value, input$num_players)
+    })
+    result <- lp("max", obj, con(), dir, rhs(), all.bin = TRUE)
+    rv$players[result$solution == 1,]
+  })
+  
+  # Define the function to run when the "update" button is pressed
+  updateLineup <- eventReactive(input$update, {
+    removedPlayer <- input$remove
+    rv$players <- rv$players[rv$players$Player != removedPlayer,]
+    obj <- rv$players$FantasyPoints
+    rhs <- reactive({
+      c(input$num_qb, input$num_rb, input$num_wr, input$num_te, input$num_value, input$num_players)
+    })
+    result <- lp("max", obj, con(), dir, rhs(), all.bin = TRUE)
+    rv$players[result$solution == 1,]
+    
+  })
+  
+  # Show the updated optimal team in a table when the "update" button is pressed
+  output$team <- renderTable({
+    if (is.null(input$remove)) {
+      initialLineup()
+    } else {
+      updateLineup()
+    }
+  })
+}
+
+
+# Run the app
+shinyApp(ui, server)
+
